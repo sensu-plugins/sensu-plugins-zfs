685031a3f7e7a0f7a5a4efb368cf4985b7ec405c
Stephen Muth <smuth4@gmail.com>
2017-11-16 12:28:52 -0500

    Properly issue critical errors first

--- bin/check-zpool.rb
+++ bin/check-zpool.rb
@@ -28,23 +28,22 @@ class CheckZPool < Sensu::Plugin::Check::CLI
          default: 7
 
   def run
+    @warnings = []
+    @criticals = []
     zpools = []
     if config[:zpool]
       zpools << SensuPluginsZFS::ZPool.new(config[:zpool])
     else
       zpools = SensuPluginsZFS::ZFS.zpools
     end
-    # Looks odd, but we want to check things that can go critical first
     zpools.each do |zp|
       check_state zp
-    end
-    zpools.each do |zp|
       check_capacity zp
-    end
-    zpools.each do |zp|
       check_vdevs zp
       check_recently_scrubbed zp
     end
+    critical @criticals.first unless @criticals.empty?
+    warning @warnings.first unless @warnings.empty?
     if config[:zpool]
       ok "zpool #{config[:zpool]} is ok"
     end
@@ -54,29 +53,29 @@ class CheckZPool < Sensu::Plugin::Check::CLI
   private
 
   def check_state(zp)
-    critical "zpool #{zp.name} has state #{zp.state}" unless zp.ok?
+    @criticals << "zpool #{zp.name} has state #{zp.state}" unless zp.ok?
   end
 
   def check_vdevs(zp)
     zp.vdevs.each do |vd|
       unless vd.ok?
-        warning "vdev #{vd.name} of zpool #{vd.zpool.name} has errors"
+        @warnings << "vdev #{vd.name} of zpool #{vd.zpool.name} has errors"
       end
     end
   end
 
   def check_capacity(zp)
     if zp.capacity > config[:cap_crit].to_i
-      critical "capacity for zpool #{zp.name} is above #{config[:cap_crit]}% (currently #{zp.capacity}%)"
+      @criticals << "capacity for zpool #{zp.name} is above #{config[:cap_crit]}% (currently #{zp.capacity}%)"
     elsif zp.capacity > config[:cap_warn].to_i
-      warning "capacity for zpool #{zp.name} is above #{config[:cap_warn]}% (currently #{zp.capacity}%)"
+      @warnings << "capacity for zpool #{zp.name} is above #{config[:cap_warn]}% (currently #{zp.capacity}%)"
     end
   end
 
   def check_recently_scrubbed(zp)
     last_scrub = zp.scrubbed_at
     if last_scrub < Time.now - 60 * 60 * 24 * config[:scrubbing_interval].to_i # rubocop:disable Style/GuardClause
-      warning "It is more than #{config[:scrubbing_interval]} days since zpool #{zp.name} was scrubbed. Last scrubbed #{last_scrub}"
+      @warnings << "It is more than #{config[:scrubbing_interval]} days since zpool #{zp.name} was scrubbed. Last scrubbed #{last_scrub}"
     end
   end
 end
